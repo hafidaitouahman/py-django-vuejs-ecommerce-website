f078389112ff71843b053fde5248798a8d36c6a4
hafidaitouahman <hafid.aitouahman>
2024-07-08 18:07:03 +0100

    increment decrement quantity on cart

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 from .cart import Cart
+import json
 # Create your views here.
 
 def cart_detail(request):
     cart = Cart(request)
     print ('cart ::::' + str(cart))
     productsstring = ''
-   # for item in cart:
-    #    productsstring += "{'id':'%s', 'title':'%s', 'quantity':'%s', 'price':'%s','total_price':'%s'}" % (item['id'], item['title'], item['quantity'], item['price'], item['total_price']) + ','
+    for item in cart:
+        product = "{'id':'%s', 'title':'%s', 'quantity':'%s', 'price':'%s','total_price':'%s'}," % (item['id'], item['title'], item['quantity'], item['price'], item['total_price'])
+        productsstring += product
 
-    #print('productsstring ::::' + productsstring)
-    #products: {{productsstring | safe}}
     context = {
         'cart': cart,
-        #'productsstring': productsstring
+        'productsstring': productsstring.rstrip(',')
     }
     return render(request,'cart_detail.html', context)
